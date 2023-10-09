68930bdab48b67e73cb2cd9365eeabb44880bf9e
pjtoral-trjn <pjtoral@usc.edu>
2023-10-09 00:49:31 -0700

    Vision Transformer

--- model/ViT/patch_embedding.py
+++ model/ViT/patch_embedding.py
@@ -39,7 +39,7 @@ class PatchEmbedding(Layer):
         )
 
     def call(self, x, training=False):
-        print(x.shape)
+        tf.print('Features shape -->', tf.shape(X), '\n')
         x = self.projection(x)
         x = tf.reshape(x,[x.shape[0],self.n_patches, self.embed_dimension]) # (n_samples, n_patches, embed_dimensions)
         return x
