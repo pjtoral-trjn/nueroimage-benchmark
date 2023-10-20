b013a3f6f533b61572d360e5438d7054becda828
brendanh189 <brendanh189@berkeley.edu>
2023-10-20 13:13:10 -0700

    renamed vgg19

--- model/VGG/VGG19.py
+++ model/VGG/VGG19.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 
-class VGG16:
+class VGG19:
   def __init__(self, args):
     self.args = args
 
@@ -24,26 +24,26 @@ class VGG16:
     images = tf.keras.Input((96, 96, 96, 1))
 
     ## First layer
-    inputs = vgg_block(images, 32, "vgg16_conv_block1", 1)
+    inputs = vgg_block(images, 32, "vgg19_conv_block1", 1)
 
     ## Second layer
-    inputs = vgg_block(images, 64, "vgg16_conv_block2", 2)
+    inputs = vgg_block(images, 64, "vgg19_conv_block2", 2)
 
     ## Third layer
-    inputs = vgg_block(images, 128, "vgg16_conv_block3", 3)
+    inputs = vgg_block(images, 128, "vgg19_conv_block3", 3)
 
     ## Fourth layer
-    inputs = vgg_block(images, 256, "vgg16_conv_block4", 4)
+    inputs = vgg_block(images, 256, "vgg19_conv_block4", 4)
 
     ## Fifth layer
-    inputs = vgg_block(images, 256, "vgg16_conv_block5", 5)
+    inputs = vgg_block(images, 256, "vgg19_conv_block5", 5)
 
     # Last Layer
     outputs = tf.keras.layers.Flatten(name="flatten")(inputs)
     outputs = tf.keras.layers.Dense(units=4096, name="fully_connected_1", activation="relu")(outputs)
     outputs = tf.keras.layers.Dense(units=4096, name="fully_connected_2", activation="relu")(outputs)
 
-    outputs = tf.keras.layers.Dense(units=1, name="Cognitive-Assessment-3DVGG16", activation="softmax")(outputs)
+    outputs = tf.keras.layers.Dense(units=1, name="Cognitive-Assessment-3DVGG19", activation="softmax")(outputs)
 
     # Define the model
-    return tf.keras.Model(images, outputs, name="3DVGG16")
+    return tf.keras.Model(images, outputs, name="3DVGG19")
