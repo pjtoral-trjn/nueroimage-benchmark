619f112fa8a04da13ad0788c580235c33e414e88
jon43 <zhiangli@usc.edu>
2023-10-22 02:56:56 -0700

    VGG19 code 1st-version upload

---
+++ model/VGG/VGG19 code.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+class VGG19_3D:
+    def __init__(self, args, train_mean, train_std):
+        self.args = args
+        self.train_mean = train_mean
+        self.train_std = train_std
+
+    def get_model(self):
+        def convolution_block(inputs, num_filters, conv_layers=2):
+            for _ in range(conv_layers):
+                inputs = tf.keras.layers.Conv3D(num_filters, 3, strides=1, padding="same", activation="relu")(inputs)
+            inputs = tf.keras.layers.MaxPooling3D(2, strides=2, padding="valid")(inputs)
+            return inputs
+
+        images = tf.keras.Input((96, 96, 96, 1))
+
+        # Block 1
+        x = convolution_block(images, 64)
+
+        # Block 2
+        x = convolution_block(x, 128)
+
+        # Block 3
+        x = convolution_block(x, 256, 4)
+
+        # Block 4
+        x = convolution_block(x, 512, 4)
+
+        # Block 5
+        x = convolution_block(x, 512, 4)
+
+        # Final layers
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dropout(rate=self.args.drop_out)(x)
+        outputs = tf.keras.layers.Dense(units=1,
+                                        bias_initializer=tf.keras.initializers.RandomNormal(
+                                            mean=self.train_mean,
+                                            stddev=self.train_std,
+                                            seed=5))(x)
+
+        return tf.keras.Model(images, outputs, name="3D_VGG19")
