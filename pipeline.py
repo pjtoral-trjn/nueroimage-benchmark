68049d113496e3b7772fb6aacb92ec644a7c3605
pjtoral-trjn <pjtoral@usc.edu>
2023-10-23 10:12:42 -0700

    saving config

--- pipeline.py
+++ pipeline.py
@@ -34,7 +34,8 @@ class Pipeline:
 
         if not os.path.exists("./output/"+self.output_filename):
             os.makedirs("./output/"+self.output_filename)
-        config_df = pd.DataFrame(eval(self.args))
+        print(self.args)
+        config_df = pd.DataFrame(vars(self.args))
         config_df.to_csv("./output/"+self.output_filename+"/config.csv", index=False)
 
     def configure_gpu(self):
